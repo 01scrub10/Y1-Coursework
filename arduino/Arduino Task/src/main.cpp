4a2649695f999d376303ced17df8982093629837
Charlie <charlie.css.2006@gmail.com>
2026-05-01 13:30:12 +0100

    function written to collect temperature data over 3 minutes

--- arduino/Arduino Task/src/main.cpp
+++ arduino/Arduino Task/src/main.cpp
@@ -1,32 +1,55 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+float* collect_temperature_data();
 int TemperatureSensorPin = A0; // Example sensor pin  
 float TemperatureReading();
-int B = 3975;
+const int B = 4275000; // B value of the thermistor
+const int R0 = 100000; // R0 = 100k
+int SamplingRateDelay = 1000;
+int SampleCollectionTime = 180000; // 180 seconds
 
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  Serial.println(result);
+  Serial.println("Starting temperature data collection...");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(TemperatureReading());
-  delay(2000);
+  float* data = collect_temperature_data();
+  for(int i = 0; i < SampleCollectionTime/SamplingRateDelay; i++)
+  {
+    Serial.print(data[i]);
+    Serial.print(", ");
+  }
+  Serial.println();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
-
-float TemperatureReading() {
-  float sensorValue = analogRead(TemperatureSensorPin);
-  float resistance = (1023-sensorValue)*10000/sensorValue; 
-  float temperature = 1/(log(resistance/10000)/B + 1/298.15) - 273.15;
+float TemperatureReading() //gets the temperature reading from the sensor and converts it to Celsius
+{
+  int a = analogRead(TemperatureSensorPin);
+  float R = 1023.0/a-1.0;
+  R = R0*R;
+  float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
+  Serial.print("temperature = ");
+  Serial.println(temperature);
+  delay(100); - 273.15;
   return temperature;
+}
+
+float* collect_temperature_data()
+{
+//collects data for 180 seconds and stores it in an array
+int Loops = SampleCollectionTime/SamplingRateDelay;
+float* temperature_data = new float[Loops];
+int i = 0;
+while(i<Loops)
+{
+temperature_data[i] = TemperatureReading();
+Serial.print(i);
+delay(SamplingRateDelay);
+i++;
+}
+return temperature_data;
 }
